90886ed30b0d630b9e8d2783fda406b6a01d1829
andrewchen8407 <andrewchen8407@gmail.com>
2019-03-31 02:30:52 -0700

    Add files via upload

---
+++ tts_gcp.py
@@ -0,0 +1,48 @@
+from google.cloud import texttospeech
+
+# Instantiates a client
+client = texttospeech.TextToSpeechClient()
+
+# Set the text input to be synthesized
+synthesis_input = texttospeech.types.SynthesisInput(text="Hello, World!")
+
+# Build the voice request, select the language code ("en-US") and the ssml
+# voice gender ("neutral")
+voice = texttospeech.types.VoiceSelectionParams(
+    language_code='en-US',
+    ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+
+# Select the type of audio file you want returned
+audio_config = texttospeech.types.AudioConfig(
+    audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+# Perform the text-to-speech request on the text input with the selected
+# voice parameters and audio file type
+response = client.synthesize_speech(synthesis_input, voice, audio_config)
+
+# The response's audio_content is binary.
+with open('output.mp3', 'wb') as out:
+    # Write the response to the output file.
+    out.write(response.audio_content)
+    print('Audio content written to file "output.mp3"')
+def synthesize_text(inputfile, outputfile):
+    """Synthesizes speech from the input string of text."""
+    client = texttospeech.TextToSpeechClient()
+    text = open(inputfile, "r").read()
+    input_text = texttospeech.types.SynthesisInput(text=text)
+
+    # Note: the voice can also be specified by name.
+    # Names of voices can be retrieved with client.list_voices().
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code='en-US',
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
+
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+    response = client.synthesize_speech(input_text, voice, audio_config)
+    # The response's audio_content is binary.
+    with open(outputfile, 'wb') as out:
+        out.write(response.audio_content)
+        print('Audio content written to file' + outputfile)
+synthesize_text("bys.txt","output1.mp3")
